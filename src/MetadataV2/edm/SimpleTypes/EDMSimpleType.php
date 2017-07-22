eb1dbd75a6d766a65ebe3851ccda77c06cf0a33b
c-harris <c.harris@communicationinnovations.com.au>
2017-07-22 08:45:05 +0000

    Apply fixes from StyleCI

--- src/MetadataV2/edm/SimpleTypes/EDMSimpleType.php
+++ src/MetadataV2/edm/SimpleTypes/EDMSimpleType.php
@@ -1,10 +1,8 @@
 <?php
 namespace AlgoWeb\ODataMetadata\MetadataV2\edm\SimpleTypes;
 
-
 use AlgoWeb\ODataMetadata\Abstracts\EDMSimpleTypeBase;
 
 class EDMSimpleType extends EDMSimpleTypeBase
 {
-
 }
